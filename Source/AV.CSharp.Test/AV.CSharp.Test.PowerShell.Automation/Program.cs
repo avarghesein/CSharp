4888844a1cbeb84ed1b366ae536bbd80b300e59b
avarghesein <abraham.varghese@hotmail.com>
2018-12-30 14:49:38 +0530

    Powershell automation

---
+++ Source/AV.CSharp.Test/AV.CSharp.Test.PowerShell.Automation/Program.cs
@@ -0,0 +1,36 @@
+﻿using AV.CSharp.PowershellAutomation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AV.CSharp.Test.PowerShell.Automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make sure you set execution policy, so that you can run scripts:
+            //i.e Set-ExecutionPolicy Unrestricted
+            //You may need to do this for both x64 and x86 version of powershell consoles
+            PowershellCallableWrapper shell = new PowershellCallableWrapper();
+
+            //Using one of my powershell commmand
+            //Download powershell modules and setup the module path accordingly
+            //https://github.com/avarghesein/Powershells/blob/master/README.md
+            dynamic results = shell.RunPowerShellCommand(
+                @"Import-Module -Name ""VA.Registry.Utility"" -Force;" + 
+                @"Search-Registry -tokenToSearch ""NGen"" -tokenType KeyName -pathsToSearch @(""HKLM\SOFTWARE\Microsoft"")");
+
+            foreach(dynamic psObject in results)
+            {
+                Console.WriteLine("KeyPath= {0}, Name = {1}, Value = {2}", psObject.KeyPath, psObject.ValueName, psObject.Value);
+            }
+
+            shell.Dispose();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Source/AV.CSharp/AV.CSharp.PowershellAutomation/PowershellCallableWrapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+
+namespace AV.CSharp.PowershellAutomation
+{
+    public class PowershellCallableWrapper
+    {
+        private RunspacePool _threadPool;
+
+        public bool OpenPowershellThreadPool()
+        {
+            var threadPool = RunspaceFactory.CreateRunspacePool();
+            threadPool.ThreadOptions = PSThreadOptions.UseCurrentThread;
+            threadPool.Open();
+            _threadPool = threadPool;
+            return true;
+        }
+
+        public dynamic RunPowerShellCommand(string command, params object[] arguments)
+        {
+            if (_threadPool == null) OpenPowershellThreadPool();
+
+            var shell = PowerShell.Create();
+            shell.RunspacePool = _threadPool;
+            shell.AddScript(command);
+            var result = shell.Invoke();
+            bool isError = shell.HadErrors;
+            shell.Dispose();
+
+            if (isError)
+            {
+                throw new ApplicationException("Error invoking script");
+            }       
+                 
+            return result;
+        }
+
+        public void Dispose()
+        {
+            if (_threadPool != null)
+            {
+                _threadPool.Close();
+                _threadPool.Dispose();
+                _threadPool = null;
+            }
+        }
+    };
+}
